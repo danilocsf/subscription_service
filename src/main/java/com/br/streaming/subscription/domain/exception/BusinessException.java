dbaebd8538c52a08515652813c764daac22fd261
Danilo Ferreira <danilocsf@gmail.com>
2026-08-05 09:12:51 -0300

    Nova exception

--- src/main/java/com/br/streaming/subscription/domain/exception/BusinessException.java
+++ src/main/java/com/br/streaming/subscription/domain/exception/BusinessException.java
@@ -1,7 +1,12 @@
 package com.br.streaming.subscription.domain.exception;
 
+import java.text.MessageFormat;
+
 public class BusinessException extends RuntimeException {
     public BusinessException(String message) {
         super(message);
+    }
+    public BusinessException(String message, Object... args) {
+        super(MessageFormat.format(message, args));
     }
 }
--- src/main/java/com/br/streaming/subscription/domain/exception/NotFoundException.java
+++ src/main/java/com/br/streaming/subscription/domain/exception/NotFoundException.java
@@ -1,4 +1,11 @@
 package com.br.streaming.subscription.domain.exception;
 
-public class NotFoundException {
+public class NotFoundException extends BusinessException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Object... args) {
+        super(message, args);
+    }
 }
